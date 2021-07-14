f54dd6c267d9173d7076218f5286d3bc9948eae7
Chen Yuan <chenyuancs@163.com>
2021-07-14 10:42:51 +0800

    init project.

---
+++ src/main/java/me/chenyuan/thinkinjava/ThinkinjavaApplication.java
@@ -0,0 +1,13 @@
+package me.chenyuan.thinkinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkinjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkinjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/chenyuan/thinkinjava/ThinkinjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chenyuan.thinkinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkinjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
